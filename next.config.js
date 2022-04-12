8dfe2775ab573b6df34f78e716a7add3942d3471
n1rium <blomgrenjohnny@gmail.com>
2022-04-12 17:24:34 +0200

    Config and race build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,41 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/race-simulation/Build/race-simulation.framework.js.gz",
+        headers: [
+          {
+            key: "Content-Encoding",
+            value: "gzip",
+          },
+        ],
+      },
+      {
+        source: "/race-simulation/Build/race-simulation.wasm.gz",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/wasm",
+          },
+          {
+            key: "Content-Encoding",
+            value: "gzip",
+          },
+        ],
+      },
+      {
+        source: "/race-simulation/Build/race-simulation.data.gz",
+        headers: [
+          {
+            key: "Content-Encoding",
+            value: "gzip",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
