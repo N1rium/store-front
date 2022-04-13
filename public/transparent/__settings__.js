c386af87cd4b5bf7f79dd1c17e03d204ea579080
n1rium <blomgrenjohnny@gmail.com>
2022-04-13 21:37:16 +0200

    Transparent update

--- public/transparent/__settings__.js
+++ public/transparent/__settings__.js
@@ -8,7 +8,7 @@ CONTEXT_OPTIONS = {
     'preferWebGl2': true,
     'powerPreference': "default"
 };
-SCRIPTS = [ 77610875, 77610890, 77610865, 77610882, 77612817, 77613155 ];
+SCRIPTS = [ 77610875, 77610890, 77610865, 77610882, 77612817, 77613155, 77619088 ];
 CONFIG_FILENAME = "config.json";
 INPUT_SETTINGS = {
     useKeyboard: true,
