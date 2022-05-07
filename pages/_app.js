766c4bffbfd8598126467f7d7cd62ca5b3d663ba
n1rium <blomgrenjohnny@gmail.com>
2022-05-07 12:41:45 +0200

    GTM fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "G-X2P5QC1KB2" });
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Script, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-X2P5QC1KB2"
-        ></script>
-        <script>
-          {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-X2P5QC1KB2');
-    `}
-        </script>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
