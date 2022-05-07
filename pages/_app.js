c1c5406365be0d6def3a7cd13580bc6db46f3ab3
n1rium <blomgrenjohnny@gmail.com>
2022-05-07 12:52:22 +0200

    Fix agfain

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,6 @@
 import "../styles/globals.css";
-import { useEffect } from "react";
-import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: "G-X2P5QC1KB2" });
-  }, []);
   return <Component {...pageProps} />;
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,21 @@ import { Html, Head, Script, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-X2P5QC1KB2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-X2P5QC1KB2');
+        `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
