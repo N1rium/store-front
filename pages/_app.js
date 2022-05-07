e220a35295f9616bbf7f8afe9450d8cb21146afc
n1rium <blomgrenjohnny@gmail.com>
2022-05-07 12:58:39 +0200

    AgainnnN

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-X2P5QC1KB2"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-X2P5QC1KB2');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
       <Head></Head>
       <body>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-X2P5QC1KB2"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-X2P5QC1KB2');
-        `}
-        </Script>
         <Main />
         <NextScript />
       </body>
